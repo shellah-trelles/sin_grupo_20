30881643c3b66050909fcdb1b3728e0453c05774
Diego Risco <diego.risco@alum.udep.edu.pe>
2022-06-21 21:34:00 -0500

    se añaden las imagenes de camisetas

---
+++ data_camisetas.php
@@ -0,0 +1,36 @@
+<?php
+
+$products = array(
+    "BULLS" => array(
+        "name" => "Chicago Bulls",
+        "talla" => "Standar",
+        "price" => "250",
+        "img" => "chicago_bulls.jpg"
+    ),
+    "LAKERS" => array(
+        "name" => "Los Angeles Lakers",
+        "talla" => "Standar",
+        "price" => "278",
+        "img" => "los_angeles_lakers.jpg"
+    ),
+    "CELTICS" => array(
+        "name" => "Boston Celtics",
+        "talla" => "Standar",
+        "price" => "245",
+        "img" => "boston_celtics.jpg"
+    ),
+    "SPURS" => array(
+        "name" => "San Antonio Spurs",
+        "talla" => "Standar",
+        "price" => "197",
+        "img" => "san_antonio_spurs.jpg"
+    ),
+    "HEAT" => array(
+        "name" => "Miami Heat",
+        "talla" => "Standar",
+        "price" => "169",
+        "img" => "miami_heat.jpg"
+    )
+);
+
+ ?>
