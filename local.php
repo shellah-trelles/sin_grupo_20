8ef83b1be1fa53d8dd2491a1195de069f2cfba5a
nestor75-ms <107740014+nestor75-ms@users.noreply.github.com>
2022-06-22 22:24:01 -0500

    Add files via upload

---
+++ local.php
@@ -0,0 +1,10 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<div class="jumbotron">
+    <h1 class="display-3">Local</h1>
+    <p class="lead">Ubicanos en calle Loreto 565</p>
+    <hr class="my-2">
+</div>
+
+<?php include("template/pie.php"); ?>
